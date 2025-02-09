f9c0681eac79d33a0ab18c55b2d4ca248c595e4e
Rodrigo Marcelino <rodrigo.marcelino@casadosventos.com.br>
2025-02-09 12:42:31 -0300

    style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import './style/App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (  
+        <div className="home">
+            <div className="title">
+                <h1>CRUD - Users</h1>
+            </div>
+        </div>
+    );
+}
+ 
+export default Home;
