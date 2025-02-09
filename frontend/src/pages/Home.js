61aed7daf65fd4531083b1959972db778abdccd7
Rodrigo Marcelino <rodrigo.marcelino@casadosventos.com.br>
2025-02-09 14:16:19 -0300

    testando a user-list

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,25 @@
+import { useState, useSyncExternalStore } from "react";
+
 const Home = () => {
+
+    const [users, setUsers] = useState(
+       [ {name: 'Rodrigo', email: ' rodrigo@casasdoventos.com.br', phone: '+55 11 974113775', id: 1},
+         {name: 'Grilinho', email: ' grilinho@casasdoventos.com.br', phone: '+55 11 8463451351', id: 2},
+       ]
+    );
+
     return (  
         <div className="home">
-            <div className="title">
-                <h1>CRUD - Users</h1>
-            </div>
+            <h1>Home</h1>
+
+            { users.map((user) => (
+                <div className="user-preview" key={user.id}>
+                    <h2>{ user.name }</h2>
+                    <p>{ user.email }</p>
+                    
+                </div>
+            ))}
+
         </div>
     );
 }
