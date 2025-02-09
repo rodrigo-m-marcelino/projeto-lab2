8995605d7e9dde5061ad2ad7ece968153c192380
Rodrigo Marcelino <rodrigo.marcelino@casadosventos.com.br>
2025-02-09 12:29:17 -0300

    navbar's content and style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
-import './style/App.css';
+import Navbar from './components/Navbar' 
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <div className="content">
+        <h1> teste </h1>
+      </div>
     </div>
   );
 }
