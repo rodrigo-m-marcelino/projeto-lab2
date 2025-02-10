e127eeacb34ac5f899c1e340c4344287c1fdd0a2
Rodrigo Marcelino <rodrigo.marcelino@casadosventos.com.br>
2025-02-10 09:25:12 -0300

    react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,26 @@
 import Navbar from './components/Navbar' 
 import Home from './pages/Home';
+import Create from './pages/Create';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; //biblioteca que permite criar rotas e controlar a navegação entre diferentes componentes sem a necessidade de carregar a página inteira
+
+// Router: torna o roteamento disponível para todos os componentes dentro dele
+// Routes: Atua como container para as rotas individuais -> Ele analisa a URL atual e renderiza o primeiro Route que corresponder ao padrão definido -> garante que uma única rota é exibida de cada vez
+// Route: Define uma rota individual
 
 function App() {
   return (
-    <div className="App">
-      <div className="navbar">
-        <Navbar />
+    <Router> 
+      <div className="App">
+          <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/create' element={<Create />}/>
+          </Routes>
+        </div>
       </div>
-      <div className="content">
-        <Home />
-      </div>
-    </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/pages/Create.js
@@ -0,0 +1,9 @@
+const Create = () => {
+    return ( 
+        <div className="tes">
+            <h1>Teste</h1>
+        </div>
+     );
+}
+ 
+export default Create;
